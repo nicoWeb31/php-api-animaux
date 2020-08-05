7a43c80c75134c1c88c63f25f0ec58f142f7c2a9
nicoWeb31 <nico.riot@free.fr>
2020-08-05 19:55:31 +0200

    sql req animal

--- models/front/ApiManager.php
+++ models/front/ApiManager.php
@@ -13,7 +13,12 @@ class ApiManager
 
     public function getDbAnimaux(){
 
-        $req = "SELECT * FROM animal";
+        $req = "SELECT * 
+        FROM animal a inner join famille f
+        on f.famille_id = a.famille_id
+        inner join animal_continent ac on ac.animal_id = a.animal_id
+        inner join continent c on c.continent = ac.continent";
+
         $stmt = $this->db->prepare($req);
 
         $stmt->execute();
